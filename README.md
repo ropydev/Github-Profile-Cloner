8da616603597cbb3c000ceba95f09ccf214ed03d
ropydev <belloconcepcionr@gmail.com>
2026-08-14 02:44:01 +0000

    v0.2: subida de repos y commits

--- README.md
+++ README.md
@@ -7,17 +7,17 @@
 
 <h3>Funciones actuales/futuras y Version del script</h3>
 
-<b>Version actual: 0.1</b>
+<b>Version actual: 0.2</b>
 
 <br>
 
 <b>Funciones Actuales</b>
-<p>El script es una base minima, hasta ahora solo automatiza el clonamiento de <b>repositorios</b> y <b>commits</b> de un perfil a una carpeta local.</p>
+<p>El script es una base minima, hasta ahora solo automatiza el clonamiento de <b>repositorios</b> y <b>commits</b> de un perfil a una carpeta local y atravez de un token de github (PAT) sube los repositorios y commits a el usuario deseado.</p>
 
 <br>
 
 <b>Funciones Futuras</b>
-<p>Esta pensado que el script clone los repos y commits como hasta ahora y utilizando un token de Github (PAT) los envie a un perfil de github seleccionado con exactamente los mismos nombres originales meno el repositorio del readme del perfil(si existe) que llevara el nombre del perfil al que se clono para que el funcionamiento sea correcto.</p>
+<p>Esta pensado que el script clone tambien la foto de perfil, descrpcion, nombre entre otros datos basicos para lograr el clonamiento mas efectivo posible.</p>
 
 <hr>
 
@@ -29,8 +29,27 @@
 git clone https://github.com/ropydev/Github-Profile-Cloner
 cd Github-Profile-Cloner
 pip install -r requirements.txt
-chmod +x main.py
-./main.py --help
+chmod +x src/main.py
+./src/main.py -v
+```
+
+<p>Si muestra la version del script (0.2) significa que se instalo correctamente.</p>
+
+<b>Uso</b>
+
+<p>Para usarlo es necesario una <b>cuenta de github</b> unicamente</p>
+<br>
+<p>En tu cuenta presiona la foto de perfil (arriba a la derecha) y cuando se abra el menu presione la opcion de Settings (o tambien llamada configuracion), en el menu lateral derecho vaya a lo ultimo y presione el apartado de de Developer Settings, una vez alli presione en el menu lateral la opcion "Personal Access Token" y dentro la opcion "Tokens(Classic)" una vez ahi presione "Generate new token" y ahi elija la opcion "Generate new token (Classic)" los permisos necesarios son "repo", "user" y "delete_repo", en caso de no estar seguro o recibir errores es recomendable seleccionar todos los permisos, despues precione en el boton verde llamado "Generate Token", una vez generado copielo y guardelo en un lugar seguro.</p>
+
+```bash
+./src/main.py -o owner -t token -u user
+```
+
+<p>Para ejecutarlo correctamente ejecute ese codigo cambiando "owner" por el usuario que se va a clonar, "token" por el token ya copiado y "user" por el usuario al que se le va a enviar todo lo clonado. Importante el "user" tiene que ser el mismo al que pertenece el "token" y "owner" puede ser cualquier usuario deseado</p>
+
+Ejemplo falso:
+```bash
+./src/main.py -o mouredev -t a1b2c3d4e5f6g7h8i9j0 -u octocat
 ```
 
 <hr>
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 # Built with Love.
 # Author: ropydev
 # Github: https://github.com/ropydev/Github-Profile-Cloner
-# v0.1 - (13/08/2026)
+# v0.2 - (14/08/2026)
 
 import requests
 import subprocess
@@ -15,6 +15,8 @@ Red = "\033[1m\033[31m"
 Green = "\033[1m\033[32m"
 Blue = "\033[36m"
 Reset = "\033[0m"
+
+clonePath = os.path.join(os.path.expanduser('~'), "GithubProfileCloner")
 
 def printLogo():
     subprocess.run(["clear"], check=True)
@@ -67,49 +69,49 @@ def getRepos(owner: str) -> list:
 def cloneMirror(repo: str, destPath: str):
     try:
         url = f"https://github.com/{repo}"
+        os.makedirs(destPath, exist_ok=True)
         subprocess.run(["git", "clone", "--mirror", url, os.path.join(destPath, repo.split("/")[1])], check=True)
         print(f"{Green}[+]{Reset} Se clono con exito el repositorio https://github.com/{repo}")
     except Exception as e:
         print(f"{Red}[!]{Reset} Ocurrio un error clonando el repositorio https://github.com/{repo}")
 
-def genUserInfo(owner: str, folder: str):
-    """
-    Obtiene información de un usuario de GitHub y la guarda en un archivo Markdown.
-    Incluye avatar, nombre, bio, ubicación, seguidores y repositorios públicos.
-    """
-    url = f"https://api.github.com/users/{owner}"
-    response = requests.get(url)
 
-    if response.status_code != 200:
-        raise Exception(f"Error al obtener datos: {response.status_code}")
+def pushMirror(token: str, owner: str, repo: str, path: str):
+    url = "https://api.github.com/user/repos"
+    headers = {"Authorization": f"token {token}"}
+    data = {
+        "name": repo,
+        "private": False,
+        "description": ""
+    }
 
-    data = response.json()
-
-    md_content = f"""<h1>Perfil de {data.get('login')}</h1>
-
-<img src="{data.get('avatar_url')}" alt="{data.get('login')} Avatar" width="250px" >
-
-<p><b>Nombre:</b> {data.get('name') or 'No disponible'} </p>
-<p><b>Bio:</b> {data.get('bio') or 'No disponible'} </p>
-<p><b>Ubicación:</b> {data.get('location') or 'No disponible'} </p>
-<p><b>Blog/Website:</b> {data.get('blog') or 'No disponible'} </p>
-<p><b>Seguidores:</b> {data.get('followers')} </p>
-<p><b>Siguiendo:</b> {data.get('following')} </p>
-<p><b>Repositorios Públicos:</b> {data.get('public_repos')}</p>
-"""
-
-    filename = os.path.join(folder, f"{owner}.md")
-    with open(filename, "w") as f:
-        f.write(md_content)
-    print(f"{Green}[+]{Reset} Perfil guardado en {filename}")
+    urlRepo = f"https://api.github.com/repos/{owner}/{repo}"
+    resp = requests.get(urlRepo, headers=headers)
+    if resp.status_code == 200:
+        deleteResp = requests.delete(urlRepo, headers=headers)
+    resp = requests.post(url, headers=headers, json=data)
+    if resp.status_code == 201:
+        print(f"{Green}[+]{Reset} Repositorio creado con éxito")
+        clone_url = resp.json()["clone_url"]
+    else:
+        print("Error al crear repo:", resp.json())
+        exit()
+    os.chdir(path)
+    subprocess.run(["git", "remote", "remove", "origin"], check=False)
+    subprocess.run(["git", "remote", "add", "origin", f"https://{owner}:{token}@github.com/{owner}/{repo}.git"], check=True)
+    subprocess.run(["git", "push", "--mirror", "origin"], check=True)
 
 
-def cloneProfile(owner, folder):
+
+def cloneProfile(owner, folder, token, user):
     try:
+        if os.path.exists(clonePath):
+            os.rmdir(clonePath)
         repos = getRepos(owner)
-        genUserInfo(owner, os.path.expanduser("~"))
         for repo in repos:
             cloneMirror(repo, folder)
+            repoName = repo.split("/")[1]
+            pushMirror(token, user, repoName, os.path.join(clonePath, repo.split("/")[1]))
         print(f"{Green}[+]{Reset} Todos los repositorios fueron clonados a la carpeta {folder}")
     except Exception as e:
         print(f"{Red}[!]{Reset} Ocurrio un error: {e}")
@@ -117,20 +119,24 @@ def cloneProfile(owner, folder):
 
 def argsParser():
     parser = argparse.ArgumentParser("./GithubProfileCloner.py -o <Github-user-dev>")
-    parser.add_argument("-v", "--version", help="Version del script usada.")
+    parser.add_argument("-v", "--version", help="Version del script usada.", action="store_true")
     parser.add_argument("-o", "--owner", help="Usuario al que clonarle los repos.")
+    parser.add_argument("-t", "--token", help="Token de La cuenta de github (PAT)")
+    parser.add_argument("-u", "--user", help="Usuario al que se le van a clonar los datos (el mismo del PAT)")
     return parser.parse_args()
 
 
 def main():
     try:
         args = argsParser()
+        owner = args.owner
+        token = args.token
+        user = args.user
         if args.version:
-            print("v0.1")
-        elif args.owner:
+            print("v0.2")
+        elif owner and token and user:
             printLogo()
-            owner = args.owner
-            cloneProfile(owner, os.path.join(f"{os.path.expanduser("~")}/GithubProfileCloner"))
+            cloneProfile(owner, clonePath, token, user)
         else:
             print(f"{Red}[!]{Reset} Argumentos invalidos.")
     except KeyboardInterrupt:
